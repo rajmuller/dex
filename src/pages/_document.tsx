03629e85f41fb0a22ec310e7faf0654cbd35b641
adamRajmuller <rajfta@gmail.com>
2022-04-07 14:13:02 +0200

    font m plus

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,22 +1,11 @@
-import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import { ColorModeScript } from "@chakra-ui/react";
+import NextDocument, { Head, Html, Main, NextScript } from "next/document";
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin=""
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=M+PLUS+1&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+        <Head />
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,7 +1,11 @@
 import { extendTheme } from "@chakra-ui/react";
 import { createBreakpoints } from "@chakra-ui/theme-tools";
+import "@fontsource/m-plus-1";
 
-const fonts = { mono: `'M PLUS 1', sans-serif` };
+const fonts = {
+  body: `'M PLUS 1', sans-serif`,
+  heading: `'M PLUS 1', sans-serif`,
+};
 
 const breakpoints = createBreakpoints({
   sm: "40em", //  640px
